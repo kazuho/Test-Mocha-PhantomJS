b5c5836c0b2e9ca744733a6b3611b3807215087c
Kazuho Oku <kazuhooku@gmail.com>
2013-12-10 17:02:57 +0900

    more doc

--- lib/Test/Mocha/PhantomJS.pm
+++ lib/Test/Mocha/PhantomJS.pm
@@ -82,7 +82,7 @@ __END__
 
 =head1 NAME
 
-Test::Mocha::PhantomJS - a wrapper for mocha-phantomjs
+Test::Mocha::PhantomJS - test your server code using mocha
 
 =head1 SYNOPSIS
 
@@ -96,6 +96,34 @@ Test::Mocha::PhantomJS - a wrapper for mocha-phantomjs
           ...
       },
   );
+
+=head1 DESCRIPTION
+
+Test::Mocha::PhantomJS is a wrapper of L<mocha-phantomjs>.  By using the module, it is easy to automatically test your server-side logic simply by writing the tests written using L<mocha>.
+
+=head1 USAGE
+
+=head2 test_mocha_phantomjs(%args)
+
+This is the only function exposed by the module.  When called, the function invokes the C<server> callback, and when the server starts up, invokes L<mocha-phantomjs> to run the test scripts.  Note that the function never returns.
+
+The arguments accepted by the function is as follows.
+
+=head3 server (mandatory)
+
+A callback to start the server.  The callback should start a server running at the specilied port (notified as the only argument to the callback) that should keep on running until a SIGTERM is being received.
+
+=head3 build_uri (optional)
+
+A callback for building the URL that is opened by L<mocha-phantomjs>.  If omitted, L<mocha-phantomjs> will open http://127.0.0.1:$port/index.html.
+
+=head3 max_wait (optional)
+
+Will wait for at most given seconds before checking port.  See also: L<Net::EmptyPort>.  The default value is 10 (seconds).
+
+=head3 auto_skip (optional)
+
+A boolean value indicating if the test should be skipped in case L<mocha-phantomjs> cannot be found.  The default value is false (i.e. do not skip).
 
 =head1 AUTHOR
 
